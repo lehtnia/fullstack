c875ef485318076b84c80a2014a684f734d052b2
nleht <nialehtonen@gmail.com>
2024-03-03 19:17:45 +0200

    tehtävät 1.6 eteenpäin

---
+++ osa1/osa/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <div>
+      {text}: {value}
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <span>No feedback given</span>
+      </div>
+    );
+  }
+
+  const average = (good - bad) / total
+  const positivePercentage = (good / total) * 100
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="Bad" value={bad} />
+      <StatisticLine text="All" value={total} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positive" value={`${positivePercentage}%`} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button onClick={handleGoodClick} text="Good" />
+      <Button onClick={handleNeutralClick} text="Neutral" />
+      <Button onClick={handleBadClick} text="Bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
